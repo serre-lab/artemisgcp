06223e4352dd3021d136ac0c83c4989753f2b1f2
jjbeck <jordan_becker@brown.edu>
2021-08-11 08:29:18 -0400

    containerize training

--- training/central_reservoir/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .layers import *
-from .cross_replica_batch_norm import *
-#from tpu_normalization import *
--- training/runjob.sh
+++
@@ -1,2 +0,0 @@
-export "CUDA_DEVICE_ORDER"="PCI_BUS_ID"
-python jobshed2.py --gpus 0 --vids '/cifs/data/tserre/CLPS_Serre_Lab/projects/prj_nih/prj_andrew_holmes/inference/i3d_try_ccv' --base_res_dir '/cifs/data/tserre/CLPS_Serre_Lab/projects/prj_nih/prj_andrew_holmes/inference/inference_i3d/' #4 5 6 7
