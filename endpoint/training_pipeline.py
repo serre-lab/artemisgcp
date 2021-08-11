fba522c099a97c78acc8aa28f848965d0ca9b172
jjbeck <jordan_becker@brown.edu>
2021-08-11 10:18:09 -0400

    github action for train container

--- endpoint/training_pipeline.py
+++ endpoint/training_pipeline.py
@@ -10,59 +10,45 @@ project_id = 'acbm-317517'
 region = 'US-CENTRAL1'
 pipeline_root_path = 'gs://vertex-ai-sdk-pipelines'
 
-def preprocess(json_file: str):
-  import os
-  import logging
-  logging.basicConfig(level=logging.INFO)
-  logging.info('The environment variable is: {}'.format(json_file))
-  
-  return json_file
-
-preprocess_step = comp.create_component_from_func(logg_env_function,
-                                                 base_image='gcr.io/deeplearning-platform-release/tf-gpu.1-15')
-
+#KFP pipeline. Needs name and root path where artifacts stored
 @kfp.dsl.pipeline(
-    name="automl-image-training-v2",
+    name="automl-image-inference-v2",
     pipeline_root=pipeline_root_path)
-def pipeline(project_id: str, dataset_file: str):
-    ds_op = gcc_aip.ImageDatasetCreateOp(
-        project=project_id,
-        display_name="flowers",
-        gcs_source=dataset_file,
-        import_schema_uri=aiplatform.schema.dataset.ioformat.image.single_label_classification,
+
+#Preprocess component. Input: video_uri - Output: embeddings
+#def preprocess(video_file: str) -> str:
+#    import os
+#    import logging
+#    logging.basicConfig(level=logging.INFO)
+#    logging.info('The video file is: {}'.format(video_file))
+
+#    return video_file + '.p'
+
+#Training component. Inputs: annotations_uri, embeddings_uri - Ouput: confusion matrix (not there yet but end goal)
+def traing(video_file: str, embeddings: str):
+
+    import os
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    logging.info('The video file is: {}, The embeddings file is : {}'.format(video_file, embeddings))
+
+#preprocess_step = comp.create_component_from_func(
+#    preprocess,
+#    base_image='gcr.io/deeplearning-platform-release/tf-gpu.1-15'
+#    )
+
+train_step = comp.create_component_from_func(
+    train,
+    base_image='gcr.io/deeplearning-platform-release/tf-gpu.1-15'
     )
 
-    preprocess_op = logg_env_function_op(dataset_file)
-    
-    training_job_run_op = gcc_aip.AutoMLImageTrainingJobRunOp(
-        project=project_id,
-        display_name="train-iris-automl-mbsdk-1",
-        prediction_type="classification",
-        model_type="CLOUD",
-        base_model=None,
-        dataset=ds_op.outputs["dataset"],
-        model_display_name="iris-classification-model-mbsdk",
-        training_fraction_split=0.6,
-        validation_fraction_split=0.2,
-        test_fraction_split=0.2,
-        budget_milli_node_hours=8000,
-    )
 
-    training_job_run_op.after(preprocess_op)
+def pipeline(project_id: str, video_file: str):
 
-    endpoint_op = gcc_aip.ModelDeployOp(
-        project=project_id, model=training_job_run_op.outputs["model"]
-    )
-
+    #preprocess_op = (preprocess_step(video_file).
+    #    add_node_selector_constraint('cloud.google.com/gke-accelerator', 'nvidia-tesla-k80').
+    #    set_gpu_limit(1))
+    inference_op = train_step(video_file, preprocess_op.output)
 
 compiler.Compiler().compile(pipeline_func=pipeline,
-        package_path='image_classif_pipeline.json')
-
-# api_client = AIPlatformClient(project_id=project_id, region=region)
-#
-# response = api_client.create_run_from_job_spec(
-#     'image_classif_pipeline.json',
-#     pipeline_root=pipeline_root_path,
-#     parameter_values={
-#         'project_id': project_id
-#     })
+        package_path='inference_pipeline.json')
