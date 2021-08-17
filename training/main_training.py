e454fa85f62259c542db37e4f8917826f52efff9
jjbeck <jordan_becker@brown.edu>
2021-08-17 13:15:02 -0400

    try training file print

--- training/main_training.py
+++ training/main_training.py
@@ -32,7 +32,7 @@ parser.add_argument('-t', '--trainedmodel', help='Path to trained model', requir
 args = parser.parse_args()
 
 
-download blobs to container based on argument
+#download blobs to container based on argument
 downloadData(annotation_bucket_name=args.annotation, embedding_bucket_name=args.emb)
 
 #f = open('annotations/Trap2_FC-A-1-12-Postfear_new_video_2019Y_02M_23D_05h_30m_06s_cam_6394846-0000.mp4_training_annotations.json')
