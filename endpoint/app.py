a6ad88ee7c313ae042f29ad2c15fec0f184347a6
Rohit Kakodkar <rohit_kakodkar@brown.edu>
2021-09-16 23:11:00 -0400

    Calling the pipeline

--- endpoint/app.py
+++ endpoint/app.py
@@ -17,14 +17,14 @@ app = Flask(__name__)
 def prediction():
     api_client = AIPlatformClient(project_id='acbm-317517', region='us-central1')
 
-    # response = api_client.create_run_from_job_spec(
-    # 'inference_pipeline.json',
-    # pipeline_root='gs://vertex-ai-sdk-pipelines',
-    # enable_caching = False,
-    # parameter_values={
-    #     'project_id': 'acbm-317517',
-    #     'video_file': 'gs://acbm_videos/videos/video_2019Y_04M_25D_12h_29m_13s_cam_6394837-0000_tesing.mp4',
-    # })
+    response = api_client.create_run_from_job_spec(
+    'inference_pipeline.json',
+    pipeline_root='gs://vertex-ai-sdk-pipelines',
+    enable_caching = False,
+    parameter_values={
+        'project_id': 'acbm-317517',
+        'video_file': 'gs://acbm_videos/videos/video_2019Y_04M_25D_12h_29m_13s_cam_6394837-0000_tesing.mp4',
+    })
 
 
     return {"predictions": 'help'}
