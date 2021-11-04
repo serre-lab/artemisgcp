85f7cf0a302344056260abaa52d6a5649e257549
rkakodkar <65554003+rkakodkar@users.noreply.github.com>
2021-11-04 09:40:55 -0400

    download from proper buckets

--- training/download_blobs.py
+++ training/download_blobs.py
@@ -12,25 +12,18 @@ def downloadData(annotation_bucket_name='', embedding_bucket_name=''):
     annotations_bucket = client.bucket(annotation_bucket_name)
     embeddings_bucket = client.bucket(embedding_bucket_name)
 
-    annotation_list_blobs = annotations_bucket.list_blobs()
+    annotation_list_blobs = annotations_bucket.list_blobs(prefix = "annotations/")
 
-    embedding_list_blobs = embeddings_bucket.list_blobs()
-
-
-    subprocess.run(["mkdir", "annotations"])
-
-    subprocess.run(["mkdir", "embeddings"])
+    embedding_list_blobs = embeddings_bucket.list_blobs(prefix = "embeddings/")
     
     for annotation in annotation_list_blobs:
         if annotation.name.endswith(".json"):
-            destination_uri = '{}/{}'.format('annotations', annotation.name) 
+            destination_uri = '{}'.format(annotation.name) 
             annotation.download_to_filename(destination_uri)
-            
 
-    
     for emb in embedding_list_blobs:
         if emb.name.endswith(".p") :
-            destination_uri = '{}/{}'.format('embeddings', emb.name) 
+            destination_uri = '{}'.format(emb.name) 
             emb.download_to_filename(destination_uri)
 
     
